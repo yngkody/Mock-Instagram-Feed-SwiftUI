2bcfd4d62382b0e6808e1c8b0e62aa4afb898f25
Teneala Spencer <teneala.spencer@usm.edu>
2021-07-31 01:01:40 -0400

    Initial Commit

---
+++ Mock Instagram Feed SwifUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mock Instagram Feed SwifUI
+//
+//  Created by Kody Young on 7/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mock Instagram Feed SwifUITests/Mock_Instagram_Feed_SwifUITests.swift
@@ -0,0 +1,33 @@
+//
+//  Mock_Instagram_Feed_SwifUITests.swift
+//  Mock Instagram Feed SwifUITests
+//
+//  Created by Kody Young on 7/31/21.
+//
+
+import XCTest
+@testable import Mock_Instagram_Feed_SwifUI
+
+class Mock_Instagram_Feed_SwifUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
